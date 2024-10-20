6115947b4af3c8df2ec1cd57343f87e0ba400f1f
Angel <111787331+angel6464@users.noreply.github.com>
2024-10-20 01:46:28 -0600

    Agregando Autor

--- src/componentes/Footerflotante.jsx
+++ src/componentes/Footerflotante.jsx
@@ -6,14 +6,6 @@ const Footerflotante = () => {
     <div className="footer-flotante">
       <div className="tarjeta">
         <p>Creador por Angel Alvarado</p>
-        <a 
-        href="https://www.linkedin.com/in/angel-aristides-alvarado-landaverde-322bb6246/" 
-        target="_blank" 
-        rel="noopener noreferrer"
-        className="seguir-boton"
-      >
-        Ver Perfil
-      </a>
       </div>
      
     </div>
